dc907cd0bebf96a438c4bc6a1035d03b68df0dfe
hamid <55974792+Raihanna1013@users.noreply.github.com>
2022-12-12 03:37:25 +0700

    coba commit

--- app/src/main/java/com/example/eppidbawaslu/Home.kt
+++ app/src/main/java/com/example/eppidbawaslu/Home.kt
@@ -1,23 +1,50 @@
 package com.example.eppidbawaslu
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.models.SlideModel
 
 class Home : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-        
-        
-        val SOP : ImageButton = findViewById(R.id.SOP)
-        SOP.setOnClickListener{
-            val intent = Intent(this@Home, SOP::class.java)
-            startActivity(intent)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_home)
+
+
+            val SOP : ImageButton = findViewById(R.id.SOP)
+            SOP.setOnClickListener{
+                val intent = Intent(this@Home, SOP::class.java)
+                startActivity(intent)
+            }
+
+            val Form : ImageButton = findViewById(R.id.formpermohonan)
+            SOP.setOnClickListener{
+                val intent = Intent(this@Home, Form_Permohonan_Informasi::class.java)
+                startActivity(intent)
+            }
+
+            val Cekstatus : ImageButton = findViewById(R.id.cekStatus)
+            Cekstatus.setOnClickListener{
+                val openURL= Intent(android.content.Intent.ACTION_VIEW)
+                openURL.data = Uri.parse("http://ppid.sleman.bawaslu.go.id/list/1/cek-status-permohonan-informasi/")
+
+                startActivity(openURL)
+
+            }
+
+            val WA : ImageButton = findViewById(R.id.CS)
+            WA.setOnClickListener{
+                val openURL=Intent(android.content.Intent.ACTION_VIEW)
+                //openurl ke web
+                openURL.data= Uri.parse("wa.me/+628112632284")
+            }
+
+
+
+
         }
-
-
-
-    }
 }
--- app/src/main/java/com/example/eppidbawaslu/SOP.kt
+++ app/src/main/java/com/example/eppidbawaslu/SOP.kt
@@ -1,30 +1,22 @@
 package com.example.eppidbawaslu
 
-import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class SOP : AppCompatActivity() {
+    lateinit var recyclerView : RecyclerView
+    lateinit var adapter : AdapterRecyclerView
+
+    class AdapterRecyclerView {
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sop)
-
-       val buttonSOP1 : Button = findViewById(R.id.buttonSOP1)
-
-        buttonSOP1.setOnClickListener {
-
-            val openURL=Intent(android.content.Intent.ACTION_VIEW)
-            //openurl ke web
-            openURL.data= Uri.parse("https://drive.google.com/file/d/1ZqAhCm_8_VyhN6e1Ffyov5XPvv-c9hsT/view")
-        }
-        val toolbar : Toolbar = findViewById(R.id.toolbar)
-        toolbar.setOnClickListener{
-            val intent = Intent(this@SOP, Home::class.java)
-            startActivity(intent)
-        }
     }
+
+
 }
